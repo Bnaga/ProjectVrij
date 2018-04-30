403a0b68a064dc4e3fae0a3a48a91c4253ac918a
Unknown <jasper@oprel.work>
2018-04-30 15:23:10 +0200

    Started Testing audiorecord

---
+++ Assets/MicrophoneToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneToggle : MonoBehaviour {
+
+	[SerializeField]
+	private AudioListener microphoneListener;
+	[SerializeField]
+	private AudioListener characterListener;
+
+	private AudioClip audioRecording;
+	[SerializeField]
+	private AudioSource playbackSource;
+	private bool recordingState;
+
+	void Update () {
+		recordingState = Input.GetButton("Fire1");
+
+		if (!microphoneListener.enabled && recordingState){
+			//AudioListener.GetOutputData
+		}
+		if (microphoneListener.enabled && !recordingState){
+
+		}
+		//SavWav.Save("test.wav",audioRecording);
+		
+		microphoneListener.enabled = recordingState;
+		characterListener.enabled = !recordingState;
+
+		if (Input.GetButton("Fire2")){
+			playbackSource.clip = audioRecording;
+			playbackSource.Play();
+		}
+	}
+}
---
+++ Assets/soundEmitter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundEmitter : MonoBehaviour {
+
+	public AudioClip testClip;
+	private SphereCollider soundSphere;
+	private bool playing;
+	private float soundTimer;
+	private AudioSource audioSource;
+
+
+	// Use this for initialization
+	void Start () {
+		soundSphere = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
+		soundSphere.radius=8;
+		soundSphere.isTrigger=true;
+		
+		if (GetComponent<AudioSource>()){
+			audioSource=GetComponent<AudioSource>();
+		}else{
+			audioSource=gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+			audioSource.spatialBlend=1;
+		}
+		PlaySound(testClip);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		soundTimer-=Time.deltaTime;
+		soundSphere.enabled= soundTimer>0;
+	}
+
+	void OnTriggerEnter (Collider other){
+		soundReceiver sr = other.GetComponent<soundReceiver>();
+		if (!sr) return;
+		sr.ReceiveSound(audioSource.clip);
+
+	}
+
+	public void PlaySound(AudioClip clip){
+		if (clip.length > soundTimer) soundTimer=clip.length;
+		audioSource.clip=clip;
+		audioSource.Play();
+	}
+}
