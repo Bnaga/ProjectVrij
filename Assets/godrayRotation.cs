6167a0d14a35adf927f452997f6a45e44bde9a07
Unknown <hairpasta@hotmail.com>
2018-05-03 01:30:51 +0200

    [+} Prototype Scene

---
+++ Assets/godrayRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class godrayRotation : MonoBehaviour {
+
+public float rotationSpeed = 100.0f; 
+
+
+// Update is called once per frame 
+	void Update () {
+		transform.Rotate(0,Time.deltaTime * rotationSpeed, 0, Space.World);
+	}
+}
