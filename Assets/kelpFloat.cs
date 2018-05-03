55094e2f60e9d6f347593f2225f1c4468a3ade13
Unknown <hairpasta@hotmail.com>
2018-05-03 15:19:14 +0200

    [+] godrays, basic kelp, fish vertex shader

---
+++ Assets/kelpFloat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kelpFloat : MonoBehaviour {
+	 public float floatStrength = 3.5f;
+	 private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(Vector3.up * floatStrength);
+	}
+}
---
+++ Assets/kelpJointConnector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class kelpJointConnector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+	}
+	
+
+}
