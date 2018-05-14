238c82637a339ff6ac64cc80448294daa6131275
Robbert <bnaga93@gmail.com>
2018-05-14 22:12:25 +0200

    test

---
+++ Assets/Scripts/MJStateManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MJStateManager : MonoBehaviour {
+
+    public Transform eyes;
+    public State currentState;
+
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+	// Use this for initialization
+	void Awake ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+    private void Update()
+    {
+        currentState.UpdateState(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (currentState != null && eyes != null )
+        {
+            Gizmos.color = currentState.sceneGizmoColor;
+            Gizmos.DrawWireSphere(eyes.position, 0.1f);
+        }
+    }
+}
