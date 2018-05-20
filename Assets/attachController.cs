a2c06d90741dcd2e0ec6b050808fdf78878c1629
Unknown <jasper@oprel.work>
2018-05-20 11:47:00 +0200

    [+] Steam Audio, TextMesh Pro, controller collision

---
+++ Assets/attachController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attachController : MonoBehaviour {
+
+	public GameObject controller;
+	private Rigidbody RB;
+	private FixedJoint FJ;
+	// Use this for initialization
+	void Start () {
+		RB = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+		FJ = gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
+		FJ.connectedBody = controller.GetComponent<Rigidbody>();
+		
+		transform.position=controller.transform.position;
+		transform.rotation=controller.transform.rotation;
+	}
+	
+}
