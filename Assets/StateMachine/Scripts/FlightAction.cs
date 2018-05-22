cab63f6345bd8937f2b6223774c8b860df2e44ad
Robbert <bnaga93@gmail.com>
2018-05-22 11:39:47 +0200

    start flight behavoir

---
+++ Assets/StateMachine/Scripts/FlightAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightAction : ActionScript
+{
+    public override void Act(MJStateManager stateManager)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Assets/StateMachine/Scripts/FlightDecision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Flight")]
+public class FlightDecision : Decision
+{
+    public override bool Decide(MJStateManager statemanager)
+    {
+        throw new NotImplementedException();
+    }
+}
