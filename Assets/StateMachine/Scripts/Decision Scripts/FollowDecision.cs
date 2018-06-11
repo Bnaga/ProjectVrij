632f1f64c61d0d1f44fe9ed680b8f6c6d20abe06
Unknown <BayanganNaga@hotmail.com>
2018-06-11 21:09:59 +0200

    follow state added

---
+++ Assets/StateMachine/Scripts/Decision Scripts/FollowDecision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Follow")]
+public class FollowDecision : Decision
+{
+    public override bool Decide(MJStateManager stateManager)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool CheckState(MJStateManager stateManager)
+    {
+        if (stateManager.curState == 14)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
