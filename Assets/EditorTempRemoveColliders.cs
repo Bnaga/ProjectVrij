4836130ec432722988f54d3122e4a8ef95430f2f
Unknown <jancarlpeters@gmail.com>
2018-06-14 10:54:43 +0200

    IT WORKS (cloth)

---
+++ Assets/EditorTempRemoveColliders.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorTempRemoveColliders : MonoBehaviour 
+{
+
+	// editor only temporary bug fix that looks for mesh colliders created by a Cloth Component and deletes them, leaving only the first one found.
+	#if (UNITY_EDITOR)
+
+	public int count;
+
+	private void Update()
+	{
+		MeshCollider[] colliders = GetComponents<MeshCollider>();
+
+		for (int i = 1; i < colliders.Length; i++)       
+		{
+			count++;
+			print("Destroyed " + colliders[i] + " as " + count + " destroyed.");
+			DestroyImmediate(colliders[i]);                
+		}
+
+	}
+	#endif
+
+}
