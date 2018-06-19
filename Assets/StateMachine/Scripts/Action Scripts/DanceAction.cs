9c7a5e8eff2b48ccbdb90cee220aeb4a7212a441
Unknown <BayanganNaga@hotmail.com>
2018-06-19 17:40:48 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/StateMachine/Scripts/Action Scripts/DanceAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DanceAction : ActionScript
+{
+
+    public override void Act(MJStateManager stateManager)
+    {
+        GoDance(stateManager);
+    }
+
+    void GoDance(MJStateManager stateManager)
+    {
+        stateManager.animator.SetBool("isDancing", true);
+    }
+}
