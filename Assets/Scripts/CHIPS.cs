3e2994460fd9b25a85e3b39b29126a35ef0ba1ef
Unknown <jasper@oprel.work>
2018-06-21 13:22:22 +0200

    [+] CHIPS OS

--- Assets/Scripts/CHIPS.cs
+++ Assets/Scripts/CHIPS.cs
@@ -8,6 +8,12 @@ public class CHIPS : MonoBehaviour {
 
 	public fishDictionary[] dictionaries;
 	public TextMeshPro display;
+	private string displayText;
+	private Coroutine processing;
+	private int currentScreen = 0;
+	private string[] intro = new string[] {	"LOADING C.H.I.P.S.","Computerized Hydrophonic\nImpression Procesing System","","",
+											"Loading Language Module.........","Loading Network Module..........","Loading Microphone Module.......","",
+											"[INITIALIZATION COMPLETE]","","......"};
 
 	void Awake () {
 		//reset known words
@@ -16,32 +22,11 @@ public class CHIPS : MonoBehaviour {
 					w.known = w.knownFromStart;
 				}
 			}
-			updateScreen();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		updateScreen();
+		display.text = displayText;
 	}
-
-	void OnTriggerEnter(Collider other){
-		soundCommunication comms = other.GetComponent<soundCommunication>();
-		if (!comms || !other.GetComponent<AudioListener>()) return;
-		Debug.Log("UPDATED CHIPS");
-		
-		/*foreach (fishDictionary.word word in comms.receivedWords){
-			foreach (fishDictionary dict in dictionaries){
-				foreach (fishDictionary.word w in dict.dictionary){
-					if (word== w) w.known = true;
-			
-				}
-			}
-		}*/
-
-		updateScreen();
-	}
-
-	
 
 	void updateScreen(){
 		string text = "Known Words:\n";
@@ -51,6 +36,43 @@ public class CHIPS : MonoBehaviour {
 					else{text+="????\n";}
 				}
 			}
-			display.SetText(text);
+			string[] lines = new string[]{text};
+			processing = StartCoroutine(ScrollingText(lines));
+	}
+
+	public void NextScreen(){
+		if (processing != null) return;
+		switch (currentScreen){
+			case 0:
+				processing = StartCoroutine(ScrollingText(intro));
+				currentScreen++;
+				return;
+			case 1:
+				updateScreen();
+				return;
+			default:
+				return;
+		}
+	}
+
+	IEnumerator ScrollingText(string[] lines){    
+		displayText =""; 
+		bool pause = false;    
+		 for (int j=0; j<lines.Length; j++){
+            int i = 0;
+            while( i < lines[j].Length ){
+				char s = lines[j][i++];
+                displayText += s;
+				if (s=='.'){
+					if (pause) yield return new WaitForSeconds(0.4f);
+					pause = true;
+				}else{ pause = false;}
+                yield return new WaitForSeconds(0.02f);
+            }
+            yield return new WaitForSeconds(.5f);
+			displayText += "\n";
+        };
+		processing=null;
+
 	}
 }
---
+++ Assets/lookingAtCHIPS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookingAtCHIPS : MonoBehaviour {
+
+	static float length = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, length))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+			CHIPS chips = hit.transform.GetComponent<CHIPS>();
+			if (chips){
+				chips.NextScreen();
+			}
+        }
+	}
+
+	
+
+}
