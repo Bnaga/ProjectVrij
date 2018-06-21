fbb1d2e687f6d072af5caaf0d7561f7c472a80cb
Tobias <hedgefoggames@outlook.com>
2018-06-21 15:25:38 +0200

    [+] Teleporting menu system

---
+++ Assets/overlayImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class overlayImage : MonoBehaviour {
+
+	public static overlayImage self;
+	private static Image overlay;
+	public static int currentAlpha;
+	// Use this for initialization
+	void Awake () {
+		overlay = GetComponent<Image>();
+		currentAlpha = Mathf.FloorToInt(overlay.color.a*255);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		self = this;
+	}
+
+	public static void setAlpha(int i){
+		Color c = overlay.color;
+		c.a = i/255f;
+		currentAlpha = i;
+		overlay.color = c;
+	}
+}
