d476a55b361111b803502298b40e8e60e3db90df
Jasper Oprel <jasper@oprel.work>
2018-06-26 00:22:15 +0200

    [+] escape pod

---
+++ Assets/lowerPod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lowerPod : MonoBehaviour {
+
+	public Vector3 Destination;
+	public Transform messageSpot;
+	public float speed;
+	private GameObject balloon;
+
+	// Update is called once per frame
+	void Update () {
+		float l = Mathf.Min(1,Vector3.Distance(transform.position,Destination));
+		transform.position = Vector3.MoveTowards(transform.position, Destination, l * speed * Time.deltaTime);
+		if (l<1 && !balloon){
+			balloon = Instantiate(knownWordManager.self.speechBalloon, messageSpot);
+			balloon.GetComponent<speechBalloon>().init("Touch pod to return to the surface");
+		}
+	}
+}
---
+++ Assets/propellorRotation.cs
@@ -0,0 +1,17 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    public class propellorRotation : MonoBehaviour
+    {
+        [Tooltip("Angular velocity in degrees per seconds")]
+        public float degPerSec = 60.0f;
+
+        // Use this for initialization
+
+        // Update is called once per frame
+        private void Update()
+        {
+            transform.RotateAround(transform.parent.transform.position, Vector3.up, degPerSec * Time.deltaTime);
+        }
+    }
+}
