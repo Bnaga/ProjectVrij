3f4ae3ab05b11c9f18f60be41dbd5f2ace247a0b
Unknown <BayanganNaga@hotmail.com>
2018-06-25 21:49:25 +0200

    fixed always dance

--- Assets/StateMachine/Scripts/Action Scripts/IdleAction.cs
+++ Assets/StateMachine/Scripts/Action Scripts/IdleAction.cs
@@ -16,6 +16,7 @@ public class IdleAction : ActionScript
 
     void RandomState(MJStateManager stateManager)
     {
+        stateManager.animator.SetBool("isDancing", true);
         //stateManager.otherIsLeader = false;
         RoleManager role = stateManager.gameObject.GetComponent<RoleManager>();
         int rand = UnityEngine.Random.Range(0, 100);
