5869e0fba247d287b71b922211ffc276579740a6
Tobias <hedgefoggames@outlook.com>
2018-06-26 14:05:20 +0200

    [/] changes world_robbert

--- Assets/lowerPod.cs
+++ Assets/lowerPod.cs
@@ -9,13 +9,29 @@ public class lowerPod : MonoBehaviour {
 	public float speed;
 	private GameObject balloon;
 
+	public float delay = 180;
+	private bool lowering = false;
+
 	// Update is called once per frame
+
+	void Start(){
+		StartCoroutine(delayTimer());
+	}
 	void Update () {
-		float l = Mathf.Min(1,Vector3.Distance(transform.position,Destination));
-		transform.position = Vector3.MoveTowards(transform.position, Destination, l * speed * Time.deltaTime);
-		if (l<1 && !balloon){
-			balloon = Instantiate(knownWordManager.self.speechBalloon, messageSpot);
-			balloon.GetComponent<speechBalloon>().init("Touch pod to return to the surface");
+		if (lowering){
+			float l = Mathf.Min(1,Vector3.Distance(transform.position,Destination));
+			transform.position = Vector3.MoveTowards(transform.position, Destination, l * speed * Time.deltaTime);
+			if (l<1 && !balloon){
+				balloon = Instantiate(knownWordManager.self.speechBalloon, messageSpot);
+				balloon.GetComponent<speechBalloon>().init("Touch pod to return to the surface");
+			}
 		}
+
+		
+	}
+
+	public IEnumerator delayTimer(){
+		yield return new WaitForSeconds(delay);
+		lowering=true;
 	}
 }
